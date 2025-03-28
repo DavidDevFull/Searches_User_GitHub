b09a6fced9316d706d2f6373c25bf6964417b9e8
David Cristiano <cristianodd3097@gmail.com>
2025-03-28 20:15:11 -0300

    Configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Searches_User_GitHub/', // Nome do repositório
+  build: {
+    outDir: 'dist',
+  },
 })
