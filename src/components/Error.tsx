2389b2293681a35326ecf25be3aec95e34cec136
David Cristiano <cristianodd3097@gmail.com>
2025-03-27 22:44:30 -0300

    Subindo projeto de pesquisa usuário github

---
+++ src/components/Error.tsx
@@ -0,0 +1,7 @@
+const Error = () => {
+    return(
+        <div><p>😓 Usuário não encontrado ❗</p></div>
+    )
+};
+
+export default Error;
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+// Obetos que serão usados na pasta home, UserProps
+export type UserProps = {
+    avatar_url:  string, // Imagem
+    login:       string, // Nome do usuário
+    location:    string, // Localização
+    followers:   number; // Seguidores
+    following:   number; // Seguindo
+}
